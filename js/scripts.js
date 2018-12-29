786bd7e9694b274686f795680a97830c693e569b
dalia hernandez <daliahdzr98@gmail.com>
2018-12-29 14:37:41 -0800

    reverse the order in the array then make into a string by using .join method

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
     var arraySentence = sentence.split(' ');//seperates words in a string and makes a new array with user input
     var newArray = arraySentence.filter(word => word.length >3);//The filter() method creates a new array with all elements that pass the test implemented by the provided function.
 
-    $('.word').text(newArray)
+    var reverse = newArray.reverse();//last word entered is the first one to appear
+    var string = reverse.join()//creates and returns a new string by concatenating all of the elements in an array 
+    $('.word').text(string)
 
     event.preventDefault();
   });
